17a70f97bb0f773d3be588a8806353be40e735fa
aspov <asp73rus@mail.ru>
2020-07-05 10:36:02 +0400

    update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->integer('check_lists_limit')->default(2);
-            $table->integer('status')->default('active');
+            $table->string('status')->default('active');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
